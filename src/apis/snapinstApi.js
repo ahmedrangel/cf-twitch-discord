562e63972afe135200bfb270dc5950d25ee87849
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-29 19:02:36 -0500

    chore: dont catch ig error

--- src/apis/snapinstApi.js
+++ src/apis/snapinstApi.js
@@ -8,26 +8,22 @@ class snapinstApi {
 
   async getMedia (link) {
     const _userAgent = randUA("desktop");
-    try {
-      const formData = new FormData();
-      formData.append("url", link);
-      formData.append("lang_code", "en");
-      const response = await fetch(this.domain, {
-        method: "POST",
-        headers: {
-          "Accept": "text/html",
-          "User-Agent": _userAgent
-        },
-        body: formData
-      });
-      const data = await response.text();
-      const html = cheerio.load(String(data));
-      const url = html("a").attr("href");
-      console.log(url);
-      return { status: 200, url: url };
-    } catch (e) {
-      return { error: e, status: 404 };
-    }
+    const formData = new FormData();
+    formData.append("url", link);
+    formData.append("lang_code", "en");
+    const response = await fetch(this.domain, {
+      method: "POST",
+      headers: {
+        "Accept": "text/html",
+        "User-Agent": _userAgent
+      },
+      body: formData
+    });
+    const data = await response.text();
+    const html = cheerio.load(String(data));
+    const url = html("a").attr("href");
+    console.log(url);
+    return { status: 200, url: url };
   }
 }
 
