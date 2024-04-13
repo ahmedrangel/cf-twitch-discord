7eef0f00d8b86b671078446ec5d0284c9d9521ba
Ahmed Rangel <ahmedrangel@outlook.com>
2024-04-13 02:44:56 -0500

    chore(igApi): readd extra post fetcher if proxy fails

--- src/apis/igApi.js
+++ src/apis/igApi.js
@@ -1,8 +1,10 @@
 import { randUA } from "@ahmedrangel/rand-user-agent";
+import * as cheerio from "cheerio";
 
 class igApi {
   constructor (ig_proxy_host) {
     this.domain = ig_proxy_host;
+    this.domain_posts = "https://fastdl.app/c/";
     this.domain_stories = "https://igram.world/api/ig/story";
   }
 
@@ -20,13 +22,32 @@ class igApi {
       return { status: 200, url: url };
     }
 
-    const response = await fetch(`${this.domain}/ig/post?url=${link}`);
-    const data = await response.json();
-    return {
-      status: 200,
-      url: data?.video_versions[0]?.url,
-      caption: data?.caption?.text
-    };
+    try {
+      const response = await fetch(`${this.domain}/ig/post?url=${link}`);
+      const data = await response.json();
+      return {
+        status: 200,
+        url: data?.video_versions[0]?.url,
+        caption: data?.caption?.text
+      };
+    } catch(e) {
+      const formData = new FormData();
+      formData.append("url", link);
+      formData.append("lang_code", "en");
+      const response = await fetch(this.domain_posts, {
+        method: "POST",
+        headers: {
+          "Accept": "text/html",
+          "User-Agent": _userAgent
+        },
+        body: formData
+      });
+      const data = await response.text();
+      const html = cheerio.load(String(data));
+      const url = html("a").attr("href");
+      console.log(url);
+      return { status: 200, url: url, caption: "" };
+    }
   }
 }
 
