ba999a52e5436c1d0e29b51cb05d40ea1b16f4c6
Ahmed Rangel <ahmedrangel@outlook.com>
2024-04-17 04:21:52 -0500

    chore: refine twitter graphql

--- src/apis/twitterApi.js
+++ src/apis/twitterApi.js
@@ -1,14 +1,61 @@
+import { randUA } from "@ahmedrangel/rand-user-agent";
+
 class twitterApi {
-  constructor(twitter_bearer_token) {
+  constructor(twitter_bearer_token, x_cookie) {
     this.twitter_bearer_token = twitter_bearer_token;
+    this.x_cookie = x_cookie;
   }
 
   async getTweet(id) {
-    const url = `https://api.twitter.com/1.1/statuses/lookup.json?id=1,${id}`;
-    const response = await fetch(url, {
-      method: "GET",
+    const _userAgent = randUA("desktop");
+    const graphql = "https://twitter.com/i/api/graphql/xOhkmRac04YFZmOzU9PJHg/TweetDetail";
+    const apiURL = new URL(graphql);
+    apiURL.searchParams.set("variables", JSON.stringify({
+      focalTweetId: id,
+      withRuxInjections: false,
+      includePromotedContent: true,
+      withCommunity: true,
+      withQuickPromoteEligibilityTweetFields: true,
+      withBirdwatchNotes: true,
+      withVoice: true,
+      withV2Timeline: true,
+    }));
+    apiURL.searchParams.set("features", JSON.stringify({
+      rweb_lists_timeline_redesign_enabled: true,
+      responsive_web_graphql_exclude_directive_enabled: true,
+      verified_phone_label_enabled: false,
+      creator_subscriptions_tweet_preview_api_enabled: true,
+      responsive_web_graphql_timeline_navigation_enabled: true,
+      responsive_web_graphql_skip_user_profile_image_extensions_enabled: false,
+      tweetypie_unmention_optimization_enabled: true,
+      responsive_web_edit_tweet_api_enabled: true,
+      graphql_is_translatable_rweb_tweet_is_translatable_enabled: true,
+      view_counts_everywhere_api_enabled: true,
+      longform_notetweets_consumption_enabled: true,
+      responsive_web_twitter_article_tweet_consumption_enabled: false,
+      tweet_awards_web_tipping_enabled: false,
+      freedom_of_speech_not_reach_fetch_enabled: true,
+      standardized_nudges_misinfo: true,
+      tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled: true,
+      longform_notetweets_rich_text_read_enabled: true,
+      longform_notetweets_inline_media_enabled: true,
+      responsive_web_media_download_video_enabled: false,
+      responsive_web_enhance_cards_enabled: false,
+    }));
+    apiURL.searchParams.set("fieldToggles", JSON.stringify({
+      withArticleRichContentState: false,
+    }));
+
+    const response = await fetch(apiURL, {
       headers: {
-        "Authorization": `Bearer ${this.twitter_bearer_token}`
+        "cookie": this.x_cookie,
+        "user-agent": _userAgent,
+        ["sec-fetch-site"]: "same-origin",
+        "Authorization": `Bearer ${this.twitter_bearer_token}`,
+        "X-Twitter-Active-User": "yes",
+        "X-Twitter-Auth-Type": "OAuth2Session",
+        "X-Twitter-Client-Language": "en",
+        "X-Csrf-Token": "0144e92ab3ad369187f2103484b8feec2d908204fdd66c97d891c468e07f7a57f2ced9f28b923d2e884f4854bf0d9b2accc07444d50a5d35b0e1afbb9bf563b926e5310e13dd28a34c098d6e3169a402"
       }
     });
     const data = await response.json();
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import y2mateApi from "./apis/y2mateApi";
 import crossclipApi from "./apis/crossclipApi";
 import fdownloaderApi from "./apis/fdownloaderApi";
 import * as cheerio from "cheerio";
-// import twitterApi from "./twitterApi";
+import twitterApi from "./apis/twitterApi";
 
 const router = IttyRouter();
 // educar
@@ -1845,26 +1845,11 @@ router.get("/dc/tiktok-video-scrapper?", async (req, env) => {
 router.get("/dc/twitter-video-scrapper?", async (req, env) => {
   const { query } = req;
   const url = decodeURIComponent(query.url);
-  const graphql = "https://twitter.com/i/api/graphql/xOhkmRac04YFZmOzU9PJHg";
   if (url.includes("twitter.com/") || url.includes("x.com/")) {
-    const _userAgent = randUA("desktop");
     console.log("es link de twitter");
+    const twitter = new twitterApi(env.twitter_bearer_token, env.x_cookie);
     const id = obtenerIDDesdeURL(url);
-    const tweetQuery = `TweetDetail?variables={"focalTweetId":"${id}","with_rux_injections":false,"includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true,"withV2Timeline":true}&features={"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}&fieldToggles={"withArticleRichContentState":false}`;
-    const eTweetQuery = encodeURI(tweetQuery);
-    const fetchTweet = await fetch(`${graphql}/${eTweetQuery}`, {
-      headers: {
-        "cookie": env.x_cookie,
-        "user-agent": _userAgent,
-        ["sec-fetch-site"]: "same-origin",
-        "Authorization": `Bearer ${env.twitter_bearer_token}`,
-        "X-Twitter-Active-User": "yes",
-        "X-Twitter-Auth-Type": "OAuth2Session",
-        "X-Twitter-Client-Language": "en",
-        "X-Csrf-Token": "0144e92ab3ad369187f2103484b8feec2d908204fdd66c97d891c468e07f7a57f2ced9f28b923d2e884f4854bf0d9b2accc07444d50a5d35b0e1afbb9bf563b926e5310e13dd28a34c098d6e3169a402"
-      }
-    });
-    const result = await fetchTweet.json();
+    const result = await twitter.getTweet(id);
     const entries = () => {
       const entries = [];
       result.data?.threaded_conversation_with_injections_v2?.instructions[0]?.entries?.forEach((en) => {
