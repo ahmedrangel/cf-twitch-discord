d120fe8c98d7faa462a614cb0f6ef48be6e14759
Ahmed Rangel <ahmedrangel@outlook.com>
2024-08-27 08:02:53 -0500

    chore: improve reddit extractor

--- src/apis/redditApi.js
+++ src/apis/redditApi.js
@@ -1,10 +1,9 @@
 import { randUA } from "@ahmedrangel/rand-user-agent";
 import { load } from "cheerio";
 import { $fetch } from "ofetch";
-import { withQuery } from "ufo";
+import { withQuery, parseURL } from "ufo";
 
 class redditApi {
-
   constructor (reddit_token) {
     this.apiBase = "https://api.reddit.com/api";
     this.downloaderBase = "https://redvid.io";
@@ -19,26 +18,25 @@ class redditApi {
       const data = await $fetch.raw(url.replace("v.redd.it", "www.reddit.com/video"), { headers }).catch((e) => console.log(e));
       url = data?.url;
     }
-
-    const regex = /\/(?:comments\/|s\/)([a-zA-Z0-9]+)/;
-    const id = url.match(regex)[1];
-
-    const { data } = await $fetch(withQuery(`${this.apiBase}/info.json`, { id: `t3_${id}` }), { headers }).catch(() => null);
-    const is_video = data?.children[0]?.data?.is_video;
-    const is_gif = data?.children[0]?.data?.url.includes(".gif");
-    const is_crosspost_video = data?.children[0]?.data?.crosspost_parent_list?.[0]?.is_video;
-    const is_crosspost_gif = data?.children[0]?.data?.crosspost_parent_list?.[0]?.url.includes(".gif");
+    const { protocol, host, pathname } = parseURL(url);
+    const jsonPage = await $fetch(`${protocol}//${host}${pathname}/.json`, { headers }).catch(() => null);
+    const { data } = jsonPage.find((item) => item?.data?.children?.[0]?.kind === "t3")?.data?.children?.[0];
+    const id = data?.id;
+    const is_video = data?.is_video;
+    const is_gif = data?.url.includes(".gif");
+    const is_crosspost_video = data?.crosspost_parent_list?.[0]?.is_video;
+    const is_crosspost_gif = data?.crosspost_parent_list?.[0]?.url.includes(".gif");
 
     if (!data || (!is_video && !is_gif && !is_crosspost_video && !is_crosspost_gif)) return null;
-    const short_url = data?.children[0]?.data?.url || data?.children[0]?.data?.crosspost_parent_list?.[0]?.url;
-    const caption = data?.children[0]?.data?.title || data?.children[0]?.data?.crosspost_parent_list?.[0]?.title;
+    const short_url = data?.url || data?.crosspost_parent_list?.[0]?.url;
+    const caption = data?.title || data?.crosspost_parent_list?.[0]?.title;
 
     if (short_url.includes(".gif")) return { id, video_url: short_url, short_url, caption, format: "gif", status: 200 };
 
-    const fallback_video = data?.children[0]?.data?.media?.reddit_video?.fallback_url || data?.children[0]?.data?.crosspost_parent_list?.[0]?.media?.reddit_video?.fallback_url;
-    if (data?.children[0]?.data?.media?.reddit_video?.is_gif || data?.children[0]?.data?.crosspost_parent_list?.[0]?.media?.reddit_video?.is_gif) return { id, video_url: fallback_video, short_url, caption, status: 200 };
+    const fallback_video = data?.media?.reddit_video?.fallback_url || data?.crosspost_parent_list?.[0]?.media?.reddit_video?.fallback_url;
+    if (data?.media?.reddit_video?.is_gif || data?.crosspost_parent_list?.[0]?.media?.reddit_video?.is_gif) return { id, video_url: fallback_video, short_url, caption, status: 200 };
 
-    const dash = data?.children[0]?.data?.media?.reddit_video?.dash_url || data?.children[0]?.data?.crosspost_parent_list?.[0]?.media?.reddit_video?.dash_url;
+    const dash = data?.media?.reddit_video?.dash_url || data?.crosspost_parent_list?.[0]?.media?.reddit_video?.dash_url;
     const xmlString = await $fetch(dash, { responseType: "text" }).catch(() => null);
     const dashAudio = xmlString.match(/<AdaptationSet[^>]+contentType="audio"[^>]*>[\s\S]+?<BaseURL>(.*?)<\/BaseURL>/)[1];
     const fallback_audio = `${short_url}/${dashAudio}`;
@@ -54,7 +52,6 @@ class redditApi {
     if (!download?.url) return null;
 
     const video_url = `${this.downloaderBase}${download?.url}`;
-
     return { id, video_url, short_url, caption, status: 200 };
   }
 }
