e818ddaa4093d808f70efacfe2c2e02bd6c10c92
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-26 21:50:49 -0500

    fix: savetube

--- src/apis/ytsavetube.js
+++ src/apis/ytsavetube.js
@@ -4,20 +4,23 @@ import { defaultRetry } from "../utils/helpers";
 
 class ytsavetubeApi {
   constructor () {
-    this.base = "https://cdn59.savetube.me";
+    this.base = "https://cdn53.savetube.su";
     this._userAgent = randUA("desktop");
   }
 
   async getVideo (id) {
     const info = await $fetch(`${this.base}/info`, {
-      params: { url: `https://www.youtube.com/watch?v=${id}` },
+      method: "POST",
+      body: { url: `https://www.youtube.com/watch?v=${id}` },
       headers: { "User-Agent": this._userAgent }
     }).catch(() => null);
 
     if (!info) return null;
 
-    const download = await $fetch(`${this.base}/download/video/720/${info.data.key}`, {
+    const download = await $fetch(`${this.base}/download`, {
       ...defaultRetry,
+      method: "POST",
+      body: { key: info.data.key, downloadType: "video", quality: "720" },
       headers: { "User-Agent": this._userAgent }
     }).catch(() => null);
 
