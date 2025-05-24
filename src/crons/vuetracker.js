74ddd24167960e01994adaa28e4aeac4381ef1b4
Ahmed Rangel <ahmedrangel@outlook.com>
2025-05-24 01:13:40 -0500

    chore: use inpur url for vuetracker lookup

--- src/crons/vuetracker.js
+++ src/crons/vuetracker.js
@@ -1,7 +1,7 @@
 import { $fetch } from "ofetch";
 
 export const vueTrackerUpdate = async (env) => {
-  const { url, updated_at } = await env.VueTracker.prepare("SELECT url, updated_at FROM sites ORDER BY updated_at ASC LIMIT 1").first();
+  const { input_url, updated_at } = await env.VueTracker.prepare("SELECT input_url, updated_at FROM sites ORDER BY updated_at ASC LIMIT 1").first();
   const today = Date.now();
   const diff = today - updated_at;
   if (diff < 1 * 24 * 60 * 60 * 1000) {
@@ -9,5 +9,5 @@ export const vueTrackerUpdate = async (env) => {
     return;
   }
   console.info(`${url} is outdated, updating...`);
-  await $fetch("https://vuetracker.pages.dev/api/lookup", { query: { url } });
+  return await $fetch("https://vuetracker.pages.dev/api/lookup", { query: { url: input_url } });
 };
