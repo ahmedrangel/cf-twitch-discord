049ed9c63a5c1fce8d977399fe5647b420d1fa01
Ahmed Rangel <ahmedrangel@outlook.com>
2025-05-25 14:26:52 -0500

    fix: update the updated_at to avoid it from getting stucked

--- src/crons/vuetracker.js
+++ src/crons/vuetracker.js
@@ -1,7 +1,8 @@
 import { $fetch } from "ofetch";
+import { defaultRetry } from "../utils/helpers";
 
 export const vueTrackerUpdate = async (env) => {
-  const { input_url, updated_at } = await env.VueTracker.prepare("SELECT input_url, updated_at FROM sites ORDER BY updated_at ASC LIMIT 1").first();
+  const { slug, input_url, updated_at } = await env.VueTracker.prepare("SELECT slug, input_url, updated_at FROM sites ORDER BY updated_at ASC LIMIT 1").first();
   const today = Date.now();
   const diff = today - updated_at;
   if (diff < 1 * 24 * 60 * 60 * 1000) {
@@ -9,5 +10,12 @@ export const vueTrackerUpdate = async (env) => {
     return;
   }
   console.info(`${input_url} is outdated, updating...`);
-  return await $fetch("https://vuetracker.pages.dev/api/lookup", { query: { url: input_url } });
+  const data = await $fetch("https://vuetracker.pages.dev/api/lookup", {
+    query: { url: input_url },
+    ...defaultRetry
+  }).catch(() => null);
+  if (!data) {
+    await env.VueTracker.prepare("UPDATE sites SET updated_at = ? WHERE slug = ?").bind(today, slug).run();
+  }
+  return data;
 };
