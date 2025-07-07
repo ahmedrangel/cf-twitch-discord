ffc879affdbf11eadef95073f7e85ad8fba9c84d
Ahmed Rangel <ahmedrangel@outlook.com>
2025-07-07 16:42:23 -0500

    fix: tiktok api

--- src/apis/tiktokApi.js
+++ src/apis/tiktokApi.js
@@ -5,7 +5,7 @@ import { defaultRetry } from "../utils/helpers";
 
 class tiktokApi {
   constructor () {
-    this.base = "https://api22-normal-c-useast2a.tiktokv.com";
+    this.base = "https://api22-normal-c-alisg.tiktokv.com";
     this.base_3rd = "https://tikwm.com/api/";
   }
 
@@ -13,6 +13,7 @@ class tiktokApi {
     if (!url?.includes("tiktok.com/")) return null;
     const method_1 = async () => {
       try {
+        console.log("Using TikTok API");
         const html = await $fetch(url, { headers: { "User-Agent": randUA("desktop") } });
         const body = load(html);
         const scripts = [];
@@ -27,33 +28,33 @@ class tiktokApi {
         const json = JSON.parse(scripts);
         const tt_id = json["__DEFAULT_SCOPE__"]["webapp.video-detail"].itemInfo.itemStruct.id;
         const device_id = json["__DEFAULT_SCOPE__"]["webapp.app-context"].wid;
-        const known_iid = ["7351144126450059040", "7351149742343391009", "7351153174894626592"];
+        const known_iid = ["7318518857994389254"];
         const query = {
-          aweme_ids: `[${tt_id}]`,
+          region: "US",
+          carrier_region: "US",
+          aweme_id: tt_id,
           iid: known_iid[Math.floor(Math.random() * known_iid.length)],
-          device_id,
+          device_id: device_id,
           channel: "googleplay",
-          aid: 1233,
           app_name: "musical_ly",
-          version_code: 350103,
-          version_name: "35.1.3",
+          version_code: "300904",
           device_platform: "android",
-          device_type: "Pixel 8 Pro",
-          os_version: 14
+          device_type: "ASUS_Z01QD",
+          os_version: "9"
         };
 
         const headers = {
           "content-type": "application/x-www-form-urlencoded",
-          "User-Agent": "com.zhiliaoapp.musically/2023501030 (Linux; U; Android 14; en_US; Pixel 8 Pro; Build/TP1A.220624.014;tt-ok/3.12.13.4-tiktok)",
-          "x-argus": ""
+          "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36"
         };
 
-        const data = await $fetch(`${this.base}/aweme/v1/multi/aweme/detail/`, { query, headers, ...defaultRetry }).catch(() => null);
+        const data = await $fetch(`${this.base}/aweme/v1/feed/`, { query, headers, ...defaultRetry }).catch(() => null);
         if (!data) return null;
 
-        const video_url = data?.aweme_details[0]?.video?.play_addr?.url_list[0];
-        const short_url = "https://m.tiktok.com/v/" + data?.aweme_details[0]?.aweme_id;
-        const caption = data?.aweme_details[0]?.desc?.trim().replace(/\s+$/, "");
+        const aweme_details = data?.aweme_list?.find((item) => item.aweme_id === tt_id);
+        const video_url = aweme_details?.video?.play_addr?.url_list?.[0] || aweme_details?.video?.download_addr?.url_list?.[0];
+        const short_url = "https://m.tiktok.com/v/" + aweme_details?.aweme_id;
+        const caption = aweme_details?.desc?.trim().replace(/\s+$/, "");
         return { id: tt_id, video_url, short_url, caption, status: 200 };
       } catch (e) {
         console.log(e);
@@ -62,6 +63,7 @@ class tiktokApi {
     };
 
     const method_2 = async () => {
+      console.log("Using TikWM API");
       const query = { url };
       const { data } = await $fetch(this.base_3rd, { query, ...defaultRetry }).catch((e) => console.log(e), null);
       if (!data) return null;
